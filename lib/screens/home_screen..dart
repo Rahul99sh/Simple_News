865a73de3a2df7fe76526f45401d0f01f6099294
Rahul99sh <109263669+Rahul99sh@users.noreply.github.com>
2024-01-06 20:27:41 +0530

    //improved code

--- lib/screens/home_screen..dart
+++ lib/screens/home_screen..dart
@@ -1,23 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:simple_news/services/news_headlines_api.dart';
-
 import '../models/article.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  List<Article> articles = [];
+  late List<Article> articles;
   bool isLoading = true;
+  bool isError = false;
+
   @override
   void initState() {
     super.initState();
-    fetchNews();
+    _fetchNews();
+  }
+
+  Future<void> _fetchNews() async {
+    try {
+      setState(() {
+        isLoading = true;
+        isError = true;
+      });
+
+      final news = await NewsHeadlines.fetchNews();
+      setState(() {
+        articles = news;
+        if(news.isEmpty){ // we are returning empty list form api class in case of error
+          isError = true;
+        }else {
+          isError = false;
+        }
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        articles = []; // Clear articles on error
+        isLoading = false;
+        isError = true;
+      });
+    }
+  }
+
+  Widget _buildArticleList() {
+      if(isError){
+        return const Center(child: Text('Error Loading the News!\nTry Again!'));
+      }else {
+        return ListView.builder(
+          itemCount: articles.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                children: [
+                  Image.network(
+                    articles[index].urlToImage,
+                  ),
+                  ListTile(
+                    title: Center(child: Text(articles[index].author)),
+                    subtitle: Text(articles[index].title),
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        articles[index].getDate,
+                      ),
+                      const SizedBox(width: 20.0)
+                    ],
+                  ),
+                  const SizedBox(height: 20.0)
+                ],
+              ),
+            );
+          },
+        );
+      }
   }
 
   @override
@@ -27,60 +91,20 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Center(
           child: Text(
             'News Headlines',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white
-            ),
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
           ),
         ),
         backgroundColor: Colors.blue,
       ),
       body: Visibility(
         visible: isLoading,
-        replacement: ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: (context, index){
-              return Card(
-                child: Column(
-                  children: [
-                    Image.network(articles[index].urlToImage),
-                    ListTile(
-                      title: Center(child: Text(articles[index].author)),
-                      subtitle: Text(articles[index].title),
-                      leading: CircleAvatar(
-                        child: Text('${index+ 1}'),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          '${articles[index].getDate}',
-                        ),
-                        const SizedBox(width: 20.0,)
-                      ],
-                    ),
-                    const SizedBox(height: 20.0,)
-                  ],
-                ),
-              );
-            }),
+        replacement: _buildArticleList(),
         child: const Center(child: CircularProgressIndicator()),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: fetchNews,
+        onPressed: _fetchNews,
         child: const Icon(Icons.refresh),
       ),
     );
-  }
-  void fetchNews() async {
-    setState(() {
-      isLoading = true;
-    });
-    final news = await NewsHeadlines.fetchNews();
-    setState(() {
-      articles = news;
-      isLoading = false;
-    });
   }
 }
--- lib/services/news_headlines_api.dart
+++ lib/services/news_headlines_api.dart
@@ -1,17 +1,35 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:simple_news/models/article.dart';
 
 class NewsHeadlines {
-  static Future<List<Article>> fetchNews() async{
-      const String url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=417cbc3d23244a98ae03446b6b5974fc';
-      final uri = Uri.parse(url);
-      final response = await http.get(uri);
-      final body = jsonDecode(response.body);
-      final result = body['articles'] as List<dynamic>;
-      final allArticles = result.map((e) {
-        return Article.fromMap(e);
-      }).toList();
-      return allArticles;
+  static const String _apiKey = '417cbc3d23244a98ae03446b6b5974fc';
+  static const String _baseUrl =
+      'https://newsapi.org/v2/top-headlines?country=in';
+
+  static Future<List<Article>> fetchNews() async {
+    try {
+      const url = '$_baseUrl&apiKey=$_apiKey';
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+        final List<dynamic> articles = body['articles'];
+        return articles.map((e) => Article.fromMap(e)).toList();
+      } else {
+        // other status codes if needed
+        if (kDebugMode) {
+          print('Failed to load news: ${response.statusCode}');
+        }
+        return []; // Return an empty list in case of failure
+      }
+    } catch (e) {
+      // e.g., network errors
+      if (kDebugMode) {
+        print('Exception while fetching news: $e');
+      }
+      return []; // Return an empty list on exception
+    }
   }
 }
